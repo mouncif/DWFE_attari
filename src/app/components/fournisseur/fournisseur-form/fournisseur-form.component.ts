60500fa5d4d54fcbe4780dec11707134ffc6a2c9
El attari Yassine <yassineattari64@gmail.com>
2020-03-07 21:59:47 +0100

    Added fournisseur CRUD & styling

--- src/app/components/fournisseur/fournisseur-form/fournisseur-form.component.ts
+++ src/app/components/fournisseur/fournisseur-form/fournisseur-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Fournisseur } from 'src/app/models/fournisseur';
+import { FournisseurService } from 'src/app/_services/fournisseur.service';
+import { Router } from '@angular/Router';
 
 @Component({
   selector: 'app-fournisseur-form',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FournisseurFormComponent implements OnInit {
 
-  constructor() { }
+  private fournisseur:Fournisseur;
+  onAddOrUpdate: boolean = true;
+
+  constructor(private fournisseurService: FournisseurService, private router: Router) { }
 
   ngOnInit() {
+    this.fournisseur = this.fournisseurService.getter();
+    console.log(this.fournisseur);
+    if(this.fournisseur.id == undefined)
+    {
+      this.onAddOrUpdate = true; 
+    }else{
+      this.onAddOrUpdate = false;
+    }
+  }
+  processForm(){
+    if(this.fournisseur.id == undefined){
+      this.onAddOrUpdate = true;
+      console.log('r');
+      this.fournisseurService.createFournisseur(this.fournisseur).subscribe(res=>{
+        console.log(res);
+        this.router.navigateByUrl('/fournisseurs');
+      });
+    }
+    else
+    {
+      this.onAddOrUpdate = false;
+      this.fournisseurService.updateFournisseur(this.fournisseur).subscribe(res=>{
+        this.router.navigateByUrl('/fournisseurs');
+        console.log(res);
+      });
+    }
   }
 
 }
--- src/app/components/fournisseur/list-fournisseur/list-fournisseur.component.ts
+++ src/app/components/fournisseur/list-fournisseur/list-fournisseur.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Fournisseur } from 'src/app/models/fournisseur';
+import { Router } from '@angular/Router';
+import { FournisseurService } from 'src/app/_services/fournisseur.service';
 
 @Component({
   selector: 'app-list-fournisseur',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListFournisseurComponent implements OnInit {
 
-  constructor() { }
+  
+  fournisseurs: Fournisseur[] = [];
+  constructor(private fournisseurService: FournisseurService, private router:Router) { }
 
   ngOnInit() {
+    this.getAllFournisseurs();
   }
 
+  getAllFournisseurs(){
+    this.fournisseurService.findAll().subscribe(res => {
+      console.log(res);
+      this.fournisseurs = res;
+    });
+  }
+
+  deleteFournisseur(fournisseur: Fournisseur){
+    this.fournisseurService.deleteFournisseur(fournisseur.id).subscribe(res=>{
+      console.log(res);
+      this.fournisseurs.splice(this.fournisseurs.indexOf(fournisseur), 1);
+    });
+  }
+
+  updateFournisseur(fournisseur: Fournisseur){
+    this.fournisseurService.setter(fournisseur);
+    this.router.navigateByUrl('/fournisseur-form');
+  }
+  newFournisseur(){
+    let fournisseur :Fournisseur;
+    this.fournisseurService.setter(fournisseur);
+    this.router.navigateByUrl('/fournisseur-form');
+  }
 }
